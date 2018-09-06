61c88ccdbfb13ddf0d9d08d5da956ec11a1be773
Evan Dorn <esdorn@stu.naperville203.org>
2018-09-06 11:47:10 -0500

    Added again another Test blueJ project file

---
+++ again another Test/Program.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Program here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Program
+{
+    public static void Main(int y)
+    {
+        System.out.println("Hello Git");
+    }
+}
